1659db2f727b70674c6442cf43cb20c8cb81f901
Marcel Noe <marcel@marcel-noe.de>
2020-08-30 16:27:01 +0200

    Initial Commit

---
+++ src/main/java/net/xcore/ressourceserver/RessourceserverApplication.java
@@ -0,0 +1,13 @@
+package net.xcore.ressourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessourceserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RessourceserverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/xcore/ressourceserver/RessourceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xcore.ressourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessourceserverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
