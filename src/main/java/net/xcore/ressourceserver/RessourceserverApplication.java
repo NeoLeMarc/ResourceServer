cb0a979d3b4c52c3fc7d5c695b32175b1f9f4fc0
Marcel Noe <marcel@marcel-noe.de>
2020-09-01 01:10:30 +0200

    Basic boilerplate for hello world application - still some problem with SpringSecurity configuration

--- src/main/java/net/xcore/ressourceserver/RessourceserverApplication.java
+++ src/main/java/net/xcore/ressourceserver/RessourceserverApplication.java
@@ -2,12 +2,20 @@ package net.xcore.ressourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RessourceserverApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RessourceserverApplication.class, args);
   }
 
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue ="World") String name){
+    return "Hello " + name + '!';
+  }
 }
