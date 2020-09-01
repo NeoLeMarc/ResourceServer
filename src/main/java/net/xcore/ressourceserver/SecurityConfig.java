d84447ef3288d63fbe950cce2407674419916706
Marcel Noe <marcel@marcel-noe.de>
2020-09-01 15:18:41 +0200

    password authentication now works

--- src/main/java/net/xcore/ressourceserver/SecurityConfig.java
+++ src/main/java/net/xcore/ressourceserver/SecurityConfig.java
@@ -1,6 +1,7 @@
 package net.xcore.ressourceserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,8 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
     }
 
-
-  private static PasswordEncoder passwordEncoder() {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 }
