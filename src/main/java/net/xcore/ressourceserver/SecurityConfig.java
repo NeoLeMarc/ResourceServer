33c146c567908d186dae6a08682a396da8bb1748
Marcel Noe <marcel@marcel-noe.de>
2020-09-05 09:23:15 +0200

    added RKI data and temporarily removed CSRF protection to enable tests with curl

--- src/main/java/net/xcore/ressourceserver/SecurityConfig.java
+++ src/main/java/net/xcore/ressourceserver/SecurityConfig.java
@@ -3,6 +3,7 @@ package net.xcore.ressourceserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,11 +15,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @SuppressWarnings("ProhibitedExceptionDeclared")
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-      auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
-    }
+    auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password"))
+        .roles("USER");
+    auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("secret"))
+        .roles("ADMIN");
+  }
+
+  @SuppressWarnings("ProhibitedExceptionDeclared")
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+  }
 
   @Bean
-  public  PasswordEncoder passwordEncoder() {
+  public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 }
