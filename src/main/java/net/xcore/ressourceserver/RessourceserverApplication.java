a8e248912e8acfd5a275ee98b4c476fe8f3bb3ca
Marcel Noe <marcel@marcel-noe.de>
2020-09-05 09:23:40 +0200

    added RKI data and temporarily removed CSRF protection to enable tests with curl

--- src/main/java/net/xcore/ressourceserver/RessourceserverApplication.java
+++ src/main/java/net/xcore/ressourceserver/RessourceserverApplication.java
@@ -1,14 +1,15 @@
 package net.xcore.ressourceserver;
 
-import java.util.List;
 import java.util.Optional;
 import net.xcore.ressourceserver.rki.domain.RkiCovid19Case;
+import net.xcore.ressourceserver.rki.domain.RkiCovid19CaseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.cassandra.core.CassandraTemplate;
-import org.springframework.data.cassandra.core.cql.CqlTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +29,13 @@ public class RessourceserverApplication {
   CassandraTemplate cassandraTemplate;
 
   @GetMapping("/rkidata")
-  public String rkidata(){
+  public String getRkidata(){
     Optional<RkiCovid19Case> rkiCase = cassandraTemplate.query(RkiCovid19Case.class).one();
     return rkiCase.get().toString();
+  }
+
+  @PostMapping(value="/rkidata", consumes = "application/json", produces = "application/json")
+  public RkiCovid19CaseDto postRkidata(@RequestBody RkiCovid19CaseDto rkiCase) {
+    return rkiCase;
   }
 }
--- src/main/java/net/xcore/ressourceserver/rki/domain/RkiCovid19CaseDto.java
+++ src/main/java/net/xcore/ressourceserver/rki/domain/RkiCovid19CaseDto.java
@@ -2,202 +2,57 @@ package net.xcore.ressourceserver.rki.domain;
 
 import java.time.Instant;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonSetter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RkiCovid19CaseDto {
-        private String altersgruppe;
-        private String altersgruppe2;
-        private int anzahlFall;
-        private int anzahlGenesen;
-        private int anzahlTodesfall;
-        private String bundesland;
-        private Instant datenstand;
-        private String geschlecht;
-        private int idBundesland;
-        private String idLandkreis;
-        private int istErkrankungsbeginn;
-        private String landkreis;
-        private Instant meldedatum;
-        private int neuGenesen;
-        private int neuerFall;
-        private int neuerTodesfall;
-        private int objectId;
-        private Instant datensatzDatum;
 
+  Logger logger = (Logger) LoggerFactory.getLogger(RkiCovid19Case.class);
 
-        @Override
-        public String toString() {
-                return "RkiCovid19CaseDto{" +
-                    "altersgruppe='" + altersgruppe + '\'' +
-                    ", altersgruppe2='" + altersgruppe2 + '\'' +
-                    ", anzahlFall=" + anzahlFall +
-                    ", anzahlGenesen=" + anzahlGenesen +
-                    ", anzahlTodesfall=" + anzahlTodesfall +
-                    ", bundesland='" + bundesland + '\'' +
-                    ", datenstand=" + datenstand +
-                    ", geschlecht='" + geschlecht + '\'' +
-                    ", idBundesland=" + idBundesland +
-                    ", idLandkreis='" + idLandkreis + '\'' +
-                    ", istErkrankungsbeginn=" + istErkrankungsbeginn +
-                    ", landkreis='" + landkreis + '\'' +
-                    ", meldedatum=" + meldedatum +
-                    ", neuGenesen=" + neuGenesen +
-                    ", neuerFall=" + neuerFall +
-                    ", neuerTodesfall=" + neuerTodesfall +
-                    ", objectId=" + objectId +
-                    ", datensatzDatum=" + datensatzDatum +
-                    '}';
-        }
+  public String Altersgruppe;
+  public String Altersgruppe2;
+  public int AnzahlFall;
+  public int AnzahlGenesen;
+  public int AnzahlTodesfall;
+  public String Bundesland;
+  public Instant Datenstand;
+  public String Geschlecht;
+  public int IdBundesland;
+  public String IdLandkreis;
+  public int IstErkrankungsbeginn;
+  public String Landkreis;
+  public Instant Meldedatum;
+  public int NeuGenesen;
+  public int NeuerFall;
+  public int NeuerTodesfall;
+  public int ObjectId;
+  public Instant DatensatzDatum;
 
-        public RkiCovid19CaseDto() {
-        }
+  public RkiCovid19CaseDto() {
+  }
 
-        @JsonProperty("Altersgruppe")
-        public String getAltersgruppe() {
-                return altersgruppe;
-        }
-
-        @JsonProperty("Altersgruppe")
-        public void setAltersgruppe(String altersgruppe) {
-                this.altersgruppe = altersgruppe;
-        }
-
-        @JsonProperty("Altersgruppe2")
-        public String getAltersgruppe2() {
-                return altersgruppe2;
-        }
-
-        @JsonProperty("Altersgruppe2")
-        public void setAltersgruppe2(String altersgruppe2) {
-                this.altersgruppe2 = altersgruppe2;
-        }
-
-        @JsonProperty("AnzahlFall")
-        public int getAnzahlFall() {
-                return anzahlFall;
-        }
-
-        @JsonProperty("AnzahlFall")
-        public void setAnzahlFall(int anzahlFall) {
-                this.anzahlFall = anzahlFall;
-        }
-                                 @JsonProperty("AnzahlGenesen")
-        public int getAnzahlGenesen() {
-                return anzahlGenesen;
-        }
-
-        public void setAnzahlGenesen(int anzahlGenesen) {
-                this.anzahlGenesen = anzahlGenesen;
-        }
-
-        public int getAnzahlTodesfall() {
-                return anzahlTodesfall;
-        }
-
-        public void setAnzahlTodesfall(int anzahlTodesfall) {
-                this.anzahlTodesfall = anzahlTodesfall;
-        }
-
-        public String getBundesland() {
-                return bundesland;
-        }
-
-        public void setBundesland(String bundesland) {
-                this.bundesland = bundesland;
-        }
-
-        public Instant getDatenstand() {
-                return datenstand;
-        }
-
-        public void setDatenstand(Instant datenstand) {
-                this.datenstand = datenstand;
-        }
-
-        public String getGeschlecht() {
-                return geschlecht;
-        }
-
-        public void setGeschlecht(String geschlecht) {
-                this.geschlecht = geschlecht;
-        }
-
-        public int getIdBundesland() {
-                return idBundesland;
-        }
-
-        public void setIdBundesland(int idBundesland) {
-                this.idBundesland = idBundesland;
-        }
-
-        public String getIdLandkreis() {
-                return idLandkreis;
-        }
-
-        public void setIdLandkreis(String idLandkreis) {
-                this.idLandkreis = idLandkreis;
-        }
-
-        public int getIstErkrankungsbeginn() {
-                return istErkrankungsbeginn;
-        }
-
-        public void setIstErkrankungsbeginn(int istErkrankungsbeginn) {
-                this.istErkrankungsbeginn = istErkrankungsbeginn;
-        }
-
-        public String getLandkreis() {
-                return landkreis;
-        }
-
-        public void setLandkreis(String landkreis) {
-                this.landkreis = landkreis;
-        }
-
-        public Instant getMeldedatum() {
-                return meldedatum;
-        }
-
-        public void setMeldedatum(Instant meldedatum) {
-                this.meldedatum = meldedatum;
-        }
-
-        public int getNeuGenesen() {
-                return neuGenesen;
-        }
-
-        public void setNeuGenesen(int neuGenesen) {
-                this.neuGenesen = neuGenesen;
-        }
-
-        public int getNeuerFall() {
-                return neuerFall;
-        }
-
-        public void setNeuerFall(int neuerFall) {
-                this.neuerFall = neuerFall;
-        }
-
-        public int getNeuerTodesfall() {
-                return neuerTodesfall;
-        }
-
-        public void setNeuerTodesfall(int neuerTodesfall) {
-                this.neuerTodesfall = neuerTodesfall;
-        }
-
-        public int getObjectId() {
-                return objectId;
-        }
-
-        public void setObjectId(int objectId) {
-                this.objectId = objectId;
-        }
-
-        public Instant getDatensatzDatum() {
-                return datensatzDatum;
-        }
-
-        public void setDatensatzDatum(Instant datensatzDatum) {
-                this.datensatzDatum = datensatzDatum;
-        }
+  @Override
+  public String toString() {
+    return "RkiCovid19CaseDto{" +
+        "Altersgruppe='" + Altersgruppe + '\'' +
+        ", Altersgruppe2='" + Altersgruppe2 + '\'' +
+        ", AnzahlFall=" + AnzahlFall +
+        ", AnzahlGenesen=" + AnzahlGenesen +
+        ", AnzahlTodesfall=" + AnzahlTodesfall +
+        ", Bundesland='" + Bundesland + '\'' +
+        ", Datenstand=" + Datenstand +
+        ", Geschlecht='" + Geschlecht + '\'' +
+        ", IdBundesland=" + IdBundesland +
+        ", IdLandkreis='" + IdLandkreis + '\'' +
+        ", IstErkrankungsbeginn=" + IstErkrankungsbeginn +
+        ", Landkreis='" + Landkreis + '\'' +
+        ", Meldedatum=" + Meldedatum +
+        ", NeuGenesen=" + NeuGenesen +
+        ", NeuerFall=" + NeuerFall +
+        ", NeuerTodesfall=" + NeuerTodesfall +
+        ", ObjectId=" + ObjectId +
+        ", DatensatzDatum=" + DatensatzDatum +
+        '}';
+  }
 }
