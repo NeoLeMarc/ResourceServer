5c2330bfe158996207eea8b6ace20ed164e6ac77
Marcel Noe <marcel@marcel-noe.de>
2020-09-12 08:58:47 +0200

    Can now query distinct DatensatzDatum from MariaDB

--- src/main/java/net/xcore/resourceserver/rki/dao/MariaDbCovid19CaseRepository.java
+++ src/main/java/net/xcore/resourceserver/rki/dao/MariaDbCovid19CaseRepository.java
@@ -1,8 +1,15 @@
 package net.xcore.resourceserver.rki.dao;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import net.xcore.resourceserver.rki.domain.mariadb.MariaDbRkiCovid19Case;
 import net.xcore.resourceserver.rki.domain.mariadb.MariaDbRkiCovid19CaseKey;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MariaDbCovid19CaseRepository extends CrudRepository<MariaDbRkiCovid19Case, MariaDbRkiCovid19CaseKey> {
+public interface MariaDbCovid19CaseRepository extends
+    CrudRepository<MariaDbRkiCovid19Case, MariaDbRkiCovid19CaseKey> {
+
+  @Query("SELECT DISTINCT(cas.caseKey.datensatzDatum) from MariaDbRkiCovid19Case as cas")
+  public List<LocalDateTime> getDistinctDatensatzDatum();
 }
---
+++ src/test/java/net/xcore/resourceserver/rki/dao/MariaDbCovid19CaseRepositoryTest.java
@@ -0,0 +1,23 @@
+package net.xcore.resourceserver.rki.dao;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MariaDbCovid19CaseRepositoryTest {
+
+  @Autowired
+  MariaDbCovid19CaseRepository repository;
+
+  @Test
+  public void testGetDistinctDatensatzDatum(){
+    List<LocalDateTime> datensatzDatumList = repository.getDistinctDatensatzDatum();
+    for(LocalDateTime datensatzDatum : datensatzDatumList){
+      System.out.println(datensatzDatum);
+    }
+  }
+
+}
