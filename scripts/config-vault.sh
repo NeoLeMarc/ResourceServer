b11227cf49c1b6fad5679865af53594d6b762755
Marcel Noe <marcel@marcel-noe.de>
2020-09-13 23:21:05 +0200

    multi environment support for vault config script

--- scripts/config-vault.sh
+++ scripts/config-vault.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
-set -e
+#!/usr/bin/zsh
 case $1 in
     dev)
+        echo "running in dev environment"
         export VAULT_ADDR='http://127.0.0.1:8200'
         vault login $VAULT_TOKEN
         ;;
     prod)
-        export VAULT_ADDR='http://127.0.0.1:8200'
-        vault login $VAULT_TOKEN
+        echo "running in prod environment"
+        unset VAULT_TOKEN
+        eval `gpg -d ~/dokumente/secret/passwords/scripts/kubemaster01_login.sh.gpg`
+        #export VAULT_ADDR='https://127.0.0.1:8200'
+        #vault login $VAULT_TOKEN
+        ;;
+    *)
+        echo "unkown environment $1"
         ;;
 esac
 
