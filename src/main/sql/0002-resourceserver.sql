fc058ff5fccc9161024903f4bc9d12549f8f302d
Marcel Noe <marcel@marcel-noe.de>
2020-09-18 22:54:05 +0200

    latest version of database structure

--- src/main/sql/0002-resourceserver.sql
+++ src/main/sql/0002-resourceserver.sql
@@ -16,6 +16,43 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Temporary table structure for view `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`
+--
+
+DROP TABLE IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`;
+/*!50001 DROP VIEW IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe` (
+  `kalenderwoche` tinyint NOT NULL,
+  `altersgruppe` tinyint NOT NULL,
+  `anzahlfaelle` tinyint NOT NULL,
+  `anzahltote` tinyint NOT NULL,
+  `fallsterblichkeit` tinyint NOT NULL,
+  `fallsterblichkeitprozent` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary table structure for view `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`
+--
+
+DROP TABLE IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`;
+/*!50001 DROP VIEW IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter` (
+  `Kalenderwoche` tinyint NOT NULL,
+  `AnzahlFaelle` tinyint NOT NULL,
+  `AnzahlTote` tinyint NOT NULL,
+  `Fallsterblichkeit` tinyint NOT NULL,
+  `FallsterblichkeitProzent` tinyint NOT NULL,
+  `DurchschnittsalterFaelle` tinyint NOT NULL,
+  `DurchschnittsalterTodesfaelle` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Temporary table structure for view `current_rki_covid19_case`
 --
 
@@ -112,15 +149,6 @@ CREATE TABLE `rki_covid19_case` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `rki_covid19_case`
---
-
-LOCK TABLES `rki_covid19_case` WRITE;
-/*!40000 ALTER TABLE `rki_covid19_case` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rki_covid19_case` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Temporary table structure for view `rki_covid19_fallsterblichkeit`
 --
 
@@ -173,6 +201,86 @@ SET character_set_client = utf8;
   `FallsterblichkeitProzent` tinyint NOT NULL
 ) ENGINE=MyISAM */;
 SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary table structure for view `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`
+--
+
+DROP TABLE IF EXISTS `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`;
+/*!50001 DROP VIEW IF EXISTS `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter` (
+  `DatensatzDatum` tinyint NOT NULL,
+  `Kalenderwoche` tinyint NOT NULL,
+  `AnzahlFaelle` tinyint NOT NULL,
+  `AnzahlTote` tinyint NOT NULL,
+  `Fallsterblichkeit` tinyint NOT NULL,
+  `FallsterblichkeitProzent` tinyint NOT NULL,
+  `DurchschnittsalterFaelle` tinyint NOT NULL,
+  `DurchschnittsalterTodesfaelle` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping routines for database 'resourceserver'
+--
+/*!50003 DROP FUNCTION IF EXISTS `altersgruppe2int` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `altersgruppe2int`(Altersgruppe char(10)) RETURNS int(11)
+    DETERMINISTIC
+begin     declare age int;     return     case Altersgruppe                   when 'A00-A04' then 4         when 'A05-14' then  14         when 'A15-A34' then 34         when 'A35-A59' then 59                    when 'A60-A79' then 79         when 'A80+' then 85         else 44.5     end; end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`
+--
+
+/*!50001 DROP TABLE IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`*/;
+/*!50001 DROP VIEW IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `c_rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe` AS select `rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`.`Kalenderwoche` AS `kalenderwoche`,`rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`.`Altersgruppe` AS `altersgruppe`,`rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`.`AnzahlFaelle` AS `anzahlfaelle`,`rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`.`AnzahlTote` AS `anzahltote`,`rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`.`Fallsterblichkeit` AS `fallsterblichkeit`,`rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`.`FallsterblichkeitProzent` AS `fallsterblichkeitprozent` from `resourceserver`.`rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe` where `rki_covid19_fallsterblichkeit_kalenderwoche_altersgruppe`.`DatensatzDatum` = (select max(`resourceserver`.`rki_covid19_case`.`DatensatzDatum`) from `resourceserver`.`rki_covid19_case`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`
+--
+
+/*!50001 DROP TABLE IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`*/;
+/*!50001 DROP VIEW IF EXISTS `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `c_rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter` AS select `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`Kalenderwoche` AS `Kalenderwoche`,`rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`AnzahlFaelle` AS `AnzahlFaelle`,`rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`AnzahlTote` AS `AnzahlTote`,`rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`Fallsterblichkeit` AS `Fallsterblichkeit`,`rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`FallsterblichkeitProzent` AS `FallsterblichkeitProzent`,`rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`DurchschnittsalterFaelle` AS `DurchschnittsalterFaelle`,`rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`DurchschnittsalterTodesfaelle` AS `DurchschnittsalterTodesfaelle` from `resourceserver`.`rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter` where `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`.`DatensatzDatum` = (select max(`resourceserver`.`rki_covid19_case`.`DatensatzDatum`) from `resourceserver`.`rki_covid19_case`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
 -- Final view structure for view `current_rki_covid19_case`
@@ -287,6 +395,25 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`
+--
+
+/*!50001 DROP TABLE IF EXISTS `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`*/;
+/*!50001 DROP VIEW IF EXISTS `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `rki_covid19_fallsterblichkeit_kalenderwoche_durchschnittsalter` AS select `inn`.`DatensatzDatum` AS `DatensatzDatum`,week(`inn`.`Meldedatum`) AS `Kalenderwoche`,count(0) AS `AnzahlFaelle`,sum(`inn`.`todesfall`) AS `AnzahlTote`,sum(`inn`.`todesfall`) / count(0) AS `Fallsterblichkeit`,sum(`inn`.`todesfall`) / count(0) * 100 AS `FallsterblichkeitProzent`,avg(`altersgruppe2int`(`inn`.`Altersgruppe`)) AS `DurchschnittsalterFaelle`,avg(if(`inn`.`todesfall` > 0,`altersgruppe2int`(`inn`.`Altersgruppe`),NULL)) AS `DurchschnittsalterTodesfaelle` from (select `resourceserver`.`rki_covid19_case`.`DatensatzDatum` AS `DatensatzDatum`,`resourceserver`.`rki_covid19_case`.`Meldedatum` AS `Meldedatum`,if(`resourceserver`.`rki_covid19_case`.`AnzahlTodesfall` > 0,1,0) AS `todesfall`,`resourceserver`.`rki_covid19_case`.`Altersgruppe` AS `Altersgruppe` from `resourceserver`.`rki_covid19_case`) `inn` group by `inn`.`DatensatzDatum`,week(`inn`.`Meldedatum`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -297,4 +424,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-09 15:31:22
+-- Dump completed on 2020-09-18 22:52:37
